18aa37d2ec11b366202ab49c00c13ac2bb8f95e1
Olivia Miller <Oliviamaxinemill@gmail.com>
2024-10-31 16:46:52 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('h4', {delay:500, duration:1000, opacity:0});
+
+ScrollReveal().reveal('.header-img', {delay:350, duration:2000, opacity:0, distance:'200px', origin:'bottom'});
+
+ScrollReveal().reveal('.article-body', {delay:600, duration:2000, opacity:0});
+
+ScrollReveal().reveal('.related', {delay:1000, duration:2000, opacity:0, distance:'200px', origin:'bottom'});
